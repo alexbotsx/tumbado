d14698406114ec8953a1548524a8c19b220ee98e
Xnnv2 <toscren0@gmail.com>
2024-09-21 22:48:14 -0500

    xd

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -17,7 +17,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥🕷️･ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐆𝐇𝐎𝐒𝐓 𝐁𝐎𝐓\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐆𝐇𝐎𝐒𝐓 𝐁𝐎𝐓\n\n`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
